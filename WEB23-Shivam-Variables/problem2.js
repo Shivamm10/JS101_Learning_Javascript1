f1884158ac7d2f45db81aaf61c3b04ff46159438
Shivamm10 <shivamggautam10@gmail.com>
2022-10-28 11:18:56 +0530

    problems on variables

---
+++ WEB23-Shivam-Variables/problem2.js
@@ -0,0 +1,6 @@
+let name="Shivam G Gautam";
+console.log(name);
+name="Shyamjeet";
+console.log(name);
+name="Gitawali";
+console.log(name);
---
+++ WEB23-Shivam-Variables/problem3.js
@@ -0,0 +1,6 @@
+let name="Shivam G Gautam";
+let age= 24;
+console.log(name);
+console.log(typeof (name));
+console.log(age);
+console.log(typeof (age));
